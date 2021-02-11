b15fcb2353ab08609d6193c0dcd779bc49c130f3
Hernán Valdés <hfvaldesg@gmail.com>
2021-02-11 09:58:42 +0100

    Add an id publisher

--- app.c
+++ app.c
@@ -2,11 +2,11 @@
 #include <rcl/rcl.h>
 #include <rclc/executor.h>
 #include <rclc/rclc.h>
+#include <std_msgs/msg/bool.h>
+#include <std_msgs/msg/int32.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#include <std_msgs/msg/bool.h>
-#include <std_msgs/msg/int32.h>
 #include "ir_mount.h"
 
 #ifdef ESP_PLATFORM
@@ -30,30 +30,36 @@
         }                                                                                  \
     }
 
-rcl_publisher_t publisher;
+rcl_publisher_t state_pub;
+rcl_publisher_t id_pub;
 rcl_subscription_t set_state_sub;
 rcl_subscription_t set_id_sub;
 
-std_msgs__msg__Bool msg;
-std_msgs__msg__Bool state_msg;
-std_msgs__msg__Int32 id_msg;
+std_msgs__msg__Bool pub_state_msg;
+std_msgs__msg__Int32 pub_id_msg;
+std_msgs__msg__Bool sub_state_msg;
+std_msgs__msg__Int32 sub_id_msg;
 
 ir_mount_t ir_mount;
 
 void set_state_callback(const void *msgin) {
-    const std_msgs__msg__Bool *state_msg = (const std_msgs__msg__Bool *)msgin;
-    ir_mount.is_on = state_msg->data;
+    const std_msgs__msg__Bool *sub_state_msg = (const std_msgs__msg__Bool *)msgin;
+    ir_mount.state = sub_state_msg->data;
 }
 
 void set_id_callback(const void *msgin) {
-    const std_msgs__msg__Int32 *id_msg = (const std_msgs__msg__Int32 *)msgin;
+    const std_msgs__msg__Int32 *sub_id_msg = (const std_msgs__msg__Int32 *)msgin;
+    ir_mount.id = sub_id_msg->data;
 }
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
     RCLC_UNUSED(last_call_time);
     if (timer != NULL) {
-        msg.data = ir_mount.is_on;
-        RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+        pub_state_msg.data = ir_mount.state;
+        RCSOFTCHECK(rcl_publish(&state_pub, &pub_state_msg, NULL));
+
+        pub_id_msg.data = ir_mount.id;
+        RCSOFTCHECK(rcl_publish(&id_pub, &pub_id_msg, NULL));
     }
 }
 
@@ -62,7 +68,7 @@ void appMain(void *arg) {
     rclc_support_t support;
 
     ir_mount.id = 1;
-    ir_mount.is_on = true;
+    ir_mount.state = true;
 
     // create init_options
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
@@ -71,26 +77,33 @@ void appMain(void *arg) {
     rcl_node_t node;
     RCCHECK(rclc_node_init_default(&node, "ir_mount_node", "", &support));
 
-    // create /ir_mount/is_on publisher (Bool)
+    // create /ir_mount/state publisher (Bool)
     RCCHECK(rclc_publisher_init_default(
-        &publisher,
+        &state_pub,
         &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
-        "/ir_mount/is_on"));
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, pub_state_msg, Bool),
+        "/ir_mount/state"));
+
+    // create /ir_mount/id publisher (Int32)
+    RCCHECK(rclc_publisher_init_default(
+        &id_pub,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, pub_state_msg, Bool),
+        "/ir_mount/id"));
 
     // create /ir_mount/set_state subscriber (Bool)
     RCCHECK(rclc_subscription_init_default(
-		&set_state_sub,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool),
-		"/ir_mount/set_state"));
-    
+        &set_state_sub,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, pub_state_msg, Bool),
+        "/ir_mount/set_state"));
+
     // create /ir_mount/set_id subscriber (Int32)
     RCCHECK(rclc_subscription_init_default(
-		&set_id_sub,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"/ir_mount/set_id"));
+        &set_id_sub,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, pub_state_msg, Int32),
+        "/ir_mount/set_id"));
 
     // create timer,
     rcl_timer_t timer;
@@ -104,15 +117,15 @@ void appMain(void *arg) {
     // create executor
     rclc_executor_t executor;
     RCCHECK(rclc_executor_init(
-        &executor, &support.context, 2, &allocator));
+        &executor, &support.context, 3, &allocator));
     RCCHECK(rclc_executor_add_subscription(
-        &executor, &set_state_sub, &state_msg, &set_state_callback, ON_NEW_DATA));
+        &executor, &set_state_sub, &sub_state_msg, &set_state_callback, ON_NEW_DATA));
     RCCHECK(rclc_executor_add_subscription(
-        &executor, &set_id_sub, &id_msg, &set_id_callback, ON_NEW_DATA));
+        &executor, &set_id_sub, &sub_id_msg, &set_id_callback, ON_NEW_DATA));
     RCCHECK(rclc_executor_add_timer(
         &executor, &timer));
 
-    msg.data = true;
+    pub_state_msg.data = true;
 
     while (1) {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
@@ -120,7 +133,8 @@ void appMain(void *arg) {
     }
 
     // free resources
-    RCCHECK(rcl_publisher_fini(&publisher, &node));
+    RCCHECK(rcl_publisher_fini(&id_pub, &node));
+    RCCHECK(rcl_publisher_fini(&state_pub, &node));
     RCCHECK(rcl_subscription_fini(&set_state_sub, &node));
     RCCHECK(rcl_subscription_fini(&set_id_sub, &node));
     RCCHECK(rcl_node_fini(&node));
--- ir_mount.h
+++ ir_mount.h
@@ -11,22 +11,19 @@
 
 typedef struct {
     int id;
-    bool is_on;
-}ir_info_t;
-
-typedef void set_state_func_t(bool new_state, ir_info_t *p_ir_info);
-typedef void set_id_func_t(int new_id, ir_info_t *p_ir_info);
-
-typedef struct {
-    int id;
-    bool is_on;
-    ir_info_t *p_ir_info;
-    set_state_func_t *set_state_fn;
-    set_id_func_t *set_id_fn;
+    bool state;
 } ir_mount_t;
 
-void set_state(bool new_state);
-void set_id(int new_id);
-
 ir_mount_t *IRMount_new(int id);
+
 void IRMount_destroy();
+
+void IRMount_set_state(ir_mount_t *p_ir_mount_t, bool new_state) {
+    p_ir_mount_t->state = new_state;
+}
+
+void IRMount_set_id(ir_mount_t *p_ir_mount_t, int new_id) {
+    if ((new_id > 0) && (new_id < 7)) {
+        p_ir_mount_t->id = new_id;
+    }
+}
