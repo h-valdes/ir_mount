07fbc598d0cde9df9e2ac5f21fe999e5cdc3379b
Hernán Valdés <hfvaldesg@gmail.com>
2021-02-12 10:13:33 +0100

    WIP: Add turn-on and turn-off function for the gpios

--- ir_mount.h
+++ ir_mount.h
@@ -24,14 +24,16 @@ ir_mount_t *IRMount_new(int start_id) {
     p_ir_mount->state = false;
 
     // Process the led information
-    for (int i = 0; i < 6; i++) {
-        p_ir_mount->led_info.led_gpio[i] = led_array[i];
+    for (int i = 0; i < LED_COUNT; i++) {
+        int gpio = led_array[i];
+
+        p_ir_mount->led_info.led_gpio[i] = gpio;
         p_ir_mount->led_info.led_state[i] = false;
 
         // Init all GPIO as output and with low value
-        gpio_pad_select_gpio(led_array[i]);
-        gpio_set_direction(led_array[i], GPIO_MODE_OUTPUT);
-        gpio_set_level(led_array[i], 0);
+        gpio_pad_select_gpio(gpio);
+        gpio_set_direction(gpio, GPIO_MODE_OUTPUT);
+        gpio_set_level(gpio, 0);
     }
 
     return p_ir_mount;
@@ -41,12 +43,28 @@ void IRMount_destroy(ir_mount_t *p_ir_mount) {
     free(p_ir_mount);
 }
 
-void IRMount_set_state(ir_mount_t *p_ir_mount_t, bool new_state) {
-    p_ir_mount_t->state = new_state;
+void IRMount_turn_off(ir_mount_t *p_ir_mount) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        int gpio = p_ir_mount->led_info.led_gpio[i];
+        gpio_set_level(gpio, 0);
+    }
 }
 
-void IRMount_set_id(ir_mount_t *p_ir_mount_t, int new_id) {
+void IRMount_turn_on(ir_mount_t *p_ir_mount) {
+
+}
+
+void IRMount_set_state(ir_mount_t *p_ir_mount, bool new_state) {
+    p_ir_mount->state = new_state;
+    if (new_state) {
+        IRMount_turn_on(p_ir_mount);
+    } else {
+        IRMount_turn_off(p_ir_mount);
+    }
+}
+
+void IRMount_set_id(ir_mount_t *p_ir_mount, int new_id) {
     if ((new_id > 0) && (new_id < 7)) {
-        p_ir_mount_t->id = new_id;
+        p_ir_mount->id = new_id;
     }
 }
--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,5 @@ gnome-terminal -- /bin/bash -c "source /opt/ros/foxy/setup.bash; \
 source ~/microros_ws/install/local_setup.bash; \
 cd ~/microros_ws; \
 ros2 run micro_ros_setup build_firmware.sh; \
+sleep 3; \
 ros2 run micro_ros_setup flash_firmware.sh" &
