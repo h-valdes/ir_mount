08f4f6c1d4fdd8bf0ef21fca5e67e418be68fb46
Hernán Valdés <hfvaldesg@gmail.com>
2021-02-23 19:19:06 +0100

    ir_mount: Modify the LEDs pins
    Modify the LEDs and change the way the LEDs are changed.
    The API isn't very helpful with the help function for the
    GPIOs (and what to use and when).

--- ir_mount.h
+++ ir_mount.h
@@ -1,11 +1,11 @@
 /*
     Schematic representation of the IR mount (L-shape)
-    (34)=(35)=(32)=(33)=(25)
+    (26)=(25)=(33)=(32)=(35)
                         ||
                         ||
-                        (26)
+                        (34)
 
-    LEDs 34, 25 26 are always on and set as reference 
+    LEDs 26, 35, 34 are always on and set as reference 
     (1)=(data)=(data)=(data)=(1)
                             ||
                             ||
@@ -22,17 +22,13 @@
 #include <driver/gpio.h>
 
 #define LED_COUNT 6
-int led_array[LED_COUNT] = {34, 35, 32, 33, 25, 26};
-
-typedef struct {
-    int led_gpio[LED_COUNT];
-    bool led_state[LED_COUNT];
-} led_info_t;
+uint32_t global_led_array[LED_COUNT] = {12, 14, 27, 26, 25, 33};
+// uint32_t global_led_array[LED_COUNT] = {15, 2, 0, 4, 16, 17};
 
 typedef struct {
     int id;
     bool state;
-    led_info_t led_info;
+    uint32_t led_array[LED_COUNT];
 } ir_mount_t;
 
 ir_mount_t *IRMount_new(int start_id) {
@@ -44,10 +40,9 @@ ir_mount_t *IRMount_new(int start_id) {
 
     // Process the led information
     for (int i = 0; i < LED_COUNT; i++) {
-        int gpio = led_array[i];
+        uint32_t gpio = global_led_array[i];
 
-        p_ir_mount->led_info.led_gpio[i] = gpio;
-        p_ir_mount->led_info.led_state[i] = false;
+        p_ir_mount->led_array[i] = gpio;
 
         // Init all GPIO as output and with low value
         gpio_pad_select_gpio(gpio);
@@ -64,25 +59,36 @@ void IRMount_destroy(ir_mount_t *p_ir_mount) {
 
 void IRMount_turn_off(ir_mount_t *p_ir_mount) {
     for (int i = 0; i < LED_COUNT; i++) {
-        int gpio = p_ir_mount->led_info.led_gpio[i];
+        uint32_t gpio = p_ir_mount->led_array[i];
         gpio_set_level(gpio, 0);
     }
 }
 
 void IRMount_turn_on(ir_mount_t *p_ir_mount) {
-    int ref_gpio_0 = p_ir_mount->led_info.led_gpio[0];
-    int ref_gpio_1 = p_ir_mount->led_info.led_gpio[LED_COUNT - 2];
-    int ref_gpio_2 = p_ir_mount->led_info.led_gpio[LED_COUNT - 1];
+    uint32_t ref_gpio_0 = 33;
+    uint32_t ref_gpio_1 = 25;
+    uint32_t ref_gpio_2 = 12;
 
     // Turn on the reference gpio's 
+    gpio_pad_select_gpio(ref_gpio_0);
+    gpio_set_direction(ref_gpio_0, GPIO_MODE_OUTPUT); 
     gpio_set_level(ref_gpio_0, 1);
-    gpio_set_level(ref_gpio_1, 1); 
-    gpio_set_level(ref_gpio_2, 1); 
 
-    for (int i = 1; i <= 3; i++) {
+    gpio_pad_select_gpio(ref_gpio_1);
+    gpio_set_direction(ref_gpio_1, GPIO_MODE_OUTPUT); 
+    gpio_set_level(ref_gpio_1, 1);
+
+    gpio_pad_select_gpio(ref_gpio_2);
+    gpio_set_direction(ref_gpio_2, GPIO_MODE_OUTPUT); 
+    gpio_set_level(ref_gpio_2, 1);
+
+    int state = p_ir_mount->id;
+
+    for (int i = 1; i < 4; i++) {
         // The ID can only be 1 or 0
-        int state = p_ir_mount->id; 
-        int id_gpio = p_ir_mount->led_info.led_gpio[LED_COUNT];
+        uint32_t id_gpio = p_ir_mount->led_array[LED_COUNT];
+        gpio_pad_select_gpio(id_gpio);
+        gpio_set_direction(id_gpio, GPIO_MODE_OUTPUT); 
         gpio_set_level(id_gpio, state);
     }
 }
